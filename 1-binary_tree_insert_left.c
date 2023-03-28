7454848991af20c67b431941868189f44b0a0841
Chidi <chidiamaraekejiuba@gmail.com>
2023-03-28 13:49:23 -0700

    added description for return value

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,13 +4,14 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: the value to store in the new node
+ * Return: pointer to the created node
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if(parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
